1844f7edd2bb7ac7721b491c5d06e84a075405f5
myubunto <saeedsamadyar@yahoo.com>
2022-08-14 07:17:01 -0700

    8/14/2022 18:46 PM

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+#from webdriver.chrome import ChromeDriverManager
 
-# driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())  # With WebDriver Manager
+driver = webdriver.Chrome("/home/myubunto/Documents/chromedriver")
+#driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
 
 driver.get("http://google.com")
 sleep(2)
+
 driver.find_element('name', 'q').send_keys("Wikipedia")
 sleep(3)
-driver.quit()  # TearDown
+driver.quit()
